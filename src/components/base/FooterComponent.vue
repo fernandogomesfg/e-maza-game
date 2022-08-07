add4ae53760fe21b908a24d0b2205be420a17232
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-07 16:31:34 -0700

    style: implementacao do layout

---
+++ src/components/base/FooterComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-footer class="bg-transparent">
+    <div class="q-pa-sm" >
+      <q-tabs
+        style="border-radius: 50px"
+        v-model="tab"
+        class="bg-white text-grey shadow-2"
+        indicator-color="transparent"
+        active-color="primary"
+      >
+        <q-tab name="home" icon="home" />
+        <q-tab name="mails" icon="mail" />
+        <q-tab name="alarms" icon="alarm" />
+        <q-tab name="movies" icon="movie" />
+      </q-tabs>
+    </div>
+  </q-footer>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'FooterComponent',
+
+  setup(){
+    const tab = ref('mail')
+
+
+    return{
+      tab
+    }
+  }
+})
+
+</script>
---
+++ src/components/base/HeaderComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-header class="bg-white text-primary">
+        <q-toolbar>
+          <q-btn
+            flat
+            dense
+            icon="menu"
+            class="q-mr-sm"
+          />
+
+
+          <q-toolbar-title>
+            <div class="text-center">
+              <div
+                class="text-grey-9 text-bold"
+                style="font-size: 12pt">
+                Caloiro
+              </div>
+
+              <div>
+                <q-linear-progress
+                  rounded size="17px"
+                  :value="progress"
+                  color="warning"
+                  class="q-mt-sm"
+                />
+              </div>
+
+              <div
+                style="font-size: : 20pt">
+                300 pts <span class="text-grey-9 text-bold"> / 1000 pts</span>
+              </div>
+            </div>
+          </q-toolbar-title>
+
+          <q-btn
+            flat
+            round
+            dense
+          >
+            <q-avatar>
+              <img src="https://github.com/fernandogomesfg.png">
+            </q-avatar>
+          </q-btn>
+        </q-toolbar>
+      </q-header>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'HeaderComponent',
+
+  setup () {
+    const progress = ref (.3)
+
+
+    return {
+      progress
+    }
+  }
+})
+
+</script>
