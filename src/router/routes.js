0569632bdfd3f31d681192aa83ff4aa95292407a
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-08 13:43:41 -0700

    chore: actualizacao das rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,20 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/HomeLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
       { path: '/home', component: () => import('pages/HomePage.vue') }
     ]
   },
+
+
 
   // Always leave this as last one,
   // but you can also remove it
