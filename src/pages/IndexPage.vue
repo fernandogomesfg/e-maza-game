5e5b13fe63ff22f33f428736262c8955f34ef4e4
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-08 13:35:18 -0700

    style: implementacao da primeira tela

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,99 +1,58 @@
 <template>
-  <div class="q-pa-md">
-    <q-carousel
-      arrows
-      animated
-      v-model="slide"
-      height="320px"
-      infinite
-    >
-      <q-carousel-slide name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h6">Quero progredir</div>
-          <div class="text-subtitle1">Ganhar mais pontos</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="second" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h6">Sou Maza</div>
-          <div class="text-subtitle1">eMazaGame</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="third" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h6">Sou nerd</div>
-          <div class="text-subtitle1">Vou conquistar mais pontos</div>
-        </div>
-      </q-carousel-slide>
-    </q-carousel>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <q-card-section
+        horizontal>
+        <q-img
+         height="400px"
+          src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+      </q-card-section>
+    </q-card>
+  </div>
+
+  <div
+    class="text-center text-grey-9 text-bold text-subtitle1">
+    <div>
+      Torne-se fluente a qualquer lingua
+    </div>
 
     <div>
-      <div class="row justify-end">
-        <q-btn
-          flat
-          color="primary"
-          label="Ver todos"
-        />
-      </div>
+      Agora pelo celular
     </div>
-
-    <div class="row justify-first">
-      <q-btn
-        flat
-        color="primary"
-        label="Jogos Populares"
-      />
-    </div>
-    <div class="q-pa-md q-gutter-sm">
-      <q-btn push color="white" text-color="primary" label="Nivel 1" />
-      <q-btn push color="white" text-color="primary" label="Nivel 2" />
-      <q-btn push color="white" text-color="primary" label="Nivel 3" />
-    </div>
-
-    <q-carousel
-      arrows
-      animated
-      v-model="slide"
-      height="100px"
-      infinite
-    >
-      <q-carousel-slide name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h6">Quero progredir</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="second" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h6">Sou Maza</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="third" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h6">Sou nerd</div>
-        </div>
-      </q-carousel-slide>
-    </q-carousel>
-
-
   </div>
+
+  <div class="text-center">
+    <q-btn
+    unelevated
+    rounded
+    color="green"
+    label="COMECAR AGORA"
+    to="/home"
+    />
+  </div>
+
 </template>
 
 <script>
-import { ref } from 'vue'
-
 export default {
   setup () {
+    const texto1 = 'Torne-se fluente a qualquer lingua'
+
+    const texto2 = 'Agora pelo celular'
+
     return {
-      slide: ref('first')
+      texto1,
+      texto2
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
-.custom-caption
-  text-align: center
-  padding: 1px
-  color: white
-  background-color: rgba(0, 0, 0, .3)
+.my-card
+  width: 100%
+  max-width: 350px
+
+
 </style>
