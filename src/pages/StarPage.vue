964fb27d38ed295d6b306de59188ae3938f1dded
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-09 17:30:28 -0700

    style: implementacao de uma parte da tela Star

---
+++ src/pages/StarPage.vue
@@ -0,0 +1,126 @@
+<template>
+
+  <div class="q-pl-md q-pt-md">
+    <div class="text-subtitle2 text-left">
+      Historico
+    </div>
+  </div>
+
+
+  <div class="q-pa-none" style="max-width: 350px">
+    <q-list bordered>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-subtitle2" >10 pontos</q-item-label>
+          <q-item-label caption>Samantha432</q-item-label>
+        </q-item-section>
+        <q-item-section class="text-right">
+          <q-item-section  side top>
+            <q-item-label class="text-subtitle2" >10.00 MZN</q-item-label>
+            <q-item-label caption>Samantha432</q-item-label>
+          </q-item-section>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <q-list bordered>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-subtitle2" >10 pontos</q-item-label>
+          <q-item-label caption>Samantha432</q-item-label>
+        </q-item-section>
+        <q-item-section class="text-right">
+          <q-item-section  side top>
+            <q-item-label class="text-subtitle2" >10.00 MZN</q-item-label>
+            <q-item-label caption>Samantha432</q-item-label>
+          </q-item-section>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <q-list bordered>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-subtitle2" >10 pontos</q-item-label>
+          <q-item-label caption>Samantha432</q-item-label>
+        </q-item-section>
+        <q-item-section class="text-right">
+          <q-item-section  side top>
+            <q-item-label class="text-subtitle2" >10.00 MZN</q-item-label>
+            <q-item-label caption>Samantha432</q-item-label>
+          </q-item-section>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <q-list bordered>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-subtitle2" >10 pontos</q-item-label>
+          <q-item-label caption>Samantha432</q-item-label>
+        </q-item-section>
+        <q-item-section class="text-right">
+          <q-item-section  side top>
+            <q-item-label class="text-subtitle2" >10.00 MZN</q-item-label>
+            <q-item-label caption>Samantha432</q-item-label>
+          </q-item-section>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <q-list bordered>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-subtitle2" >10 pontos</q-item-label>
+          <q-item-label caption>Samantha432</q-item-label>
+        </q-item-section>
+        <q-item-section class="text-right">
+          <q-item-section  side top>
+            <q-item-label class="text-subtitle2" >10.00 MZN</q-item-label>
+            <q-item-label caption>Samantha432</q-item-label>
+          </q-item-section>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+
+
+
+
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      progress1: 0.99,
+      progress2: 0.70
+    }
+  }
+}
+</script>
