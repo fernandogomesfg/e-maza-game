590b7fee0c9ec7416f818626ab76b18d2454117d
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-10 22:33:04 -0700

    chore: finalizacao da tela Computer

--- src/pages/ComputerPage.vue
+++ src/pages/ComputerPage.vue
@@ -38,15 +38,51 @@
     </q-scroll-area>
   </div>
 
-  <div class="col-12 q-pl-xs q-mt-lg">
+  <div class="col-12 q-pl-xs q-ma-sm q-mt-xl">
     <div class="row">
       <div class="col-6">
-        <div style="font-size: 12pt;" class="text-bold">
+        <div style="font-size: 15pt;" class="text-bold">
           Jogos Populares
         </div>
       </div>
-      <div class="col-6"></div>
+      <div class="col-6 text-right">
+        <route-link
+          to="/ss"
+          style="text-decoration: none; font-size: 12pt"
+          class="text-primary text bold"
+
+        >
+          Ver Todos
+        </route-link>
+      </div>
     </div>
+      <div class="q-gutter-md">
+        <q-btn
+          label="Nivel 1"
+          style="border-radius: 10px; font-size: 8pt;"
+          size="sm"
+          unelevated
+          color="grey-9"
+        />
+
+        <q-btn
+          label="Nivel 2"
+          style="border-radius: 10px; font-size: 8pt;"
+          size="sm"
+          unelevated
+          color="grey-4"
+          class="text-black"
+        />
+
+        <q-btn
+          label="Nivel 3"
+          style="border-radius: 10px; font-size: 8pt;"
+          size="sm"
+          unelevated
+          color="grey-4"
+          class="text-black"
+        />
+      </div>
   </div>
 
 
@@ -54,12 +90,13 @@
 
 
     <q-scroll-area
-      style="height: 150px; margin: -15px;"
+      class="q-pt-lg q-mb-lg"
+      style="height: 180px; margin: -15px;"
     >
       <div class="row no-wrap">
-        <div v-for="n in 5" :key="n" style="width: 300px; height: 150px;" class="q-pa-sm">
+        <div v-for="n in 5" :key="n" style="width: 300px; height: 180px;" class="q-pa-sm">
           <q-card class="full-height full-width" style="border-radius: 20px;">
-            <q-img src="images/main-person-image.png" style="height: 100%; width: 100%;">
+            <q-img src="images/calculadora.jpg" style="height: 100%; width: 100%;">
               <div class="full-height" style="background: rgba(0, 0, 0, 0.3);">
                 <div
                   class="text-h6"
