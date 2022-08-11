a9b23c5d6b07a69e64e2e0a9ed534e4c7dbfc1bb
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-08-10 22:58:45 -0700

    chore: actualizacao da tela star

--- src/pages/StarPage.vue
+++ src/pages/StarPage.vue
@@ -1,6 +1,30 @@
 <template>
 
-  <div class="q-pl-md q-pt-md">
+  <q-scroll-area
+      class="q-pt-lg q-mb-lg"
+      style="height: 180px; margin: -15px;"
+    >
+      <div class="row no-wrap">
+        <div v-for="n in 5" :key="n" style="width: 300px; height: 180px;" class="q-pa-sm">
+          <q-card class="full-height full-width" style="border-radius: 20px;">
+            <q-img src="images/presente.jpg" style="height: 100%; width: 100%;">
+              <div class="full-height" style="background: rgba(0, 0, 0, 0.3);">
+                <div
+                  class="text-h6"
+                  style="font-size: 25pt; line-height: 42px"
+
+                >
+                  Transferir presente
+                </div>
+
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+      </div>
+    </q-scroll-area>
+
+  <div class="q-pl-md q-mt-xl">
     <div class="text-subtitle2 text-left">
       Historico
     </div>
